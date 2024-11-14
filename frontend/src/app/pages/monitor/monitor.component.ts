b7ea4af59b8a22a7be3f196e677d318c1116152b
Victor Bejarano <victor.bejarano@sophossolutions.com>
2024-11-13 23:23:36 -0500

    Ajustes al front

--- frontend/src/app/pages/monitor/monitor.component.ts
+++ frontend/src/app/pages/monitor/monitor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { SocketService } from "./socket.service";
 import { Socket } from "../../socket";
 
@@ -7,24 +7,5 @@ import { Socket } from "../../socket";
   styleUrls: ["./monitor.component.scss"],
   templateUrl: "./monitor.component.html",
 })
-export class MonitorComponent implements OnInit, OnDestroy {
-  data: any;
-
-  constructor(private socket: Socket) {}
-
-  ngOnInit() {
-    // // Escuchar el evento 'data_update' del servidor Flask
-    // this.socket
-    //   .fromEvent("data_update")
-    //   .subscribe((data) => {
-    //     console.log("Datos recibidos:", data);
-    //     this.data = data;
-    //   });
-
-    // // Emitir un evento de ejemplo
-    // this.socket.emit('start_bluetooth_stream', '');
-  }
-
-  ngOnDestroy() {
-  }
+export class MonitorComponent {
 }
