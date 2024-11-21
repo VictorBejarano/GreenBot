b60199d98265141ec419e4ea2d33ffdf3c644df9
Victor Bejarano <victor.bejarano@sophossolutions.com>
2024-11-20 23:11:27 -0500

    view data saved

--- frontend/src/app/pages/monitor/monitor.module.ts
+++ frontend/src/app/pages/monitor/monitor.module.ts
@@ -13,8 +13,8 @@ import { HumidityCardComponent } from "./humidity-card/humidity-card.component";
 import { SoilMoistureCardComponent } from "./soil-moisture-card/soil-moisture-card.component";
 import { TemperatureBCardComponent } from "./temperature-b-card/temperature-b-card.component";
 import { QualityCardComponent } from "./quality-card/quality-card.component";
-const config: SocketIoConfig = { url: "http://192.168.10.3:5000", options: {} };
-// const config: SocketIoConfig = { url: "http://192.168.4.1:5000", options: {} };
+// const config: SocketIoConfig = { url: "http://192.168.10.3:5000", options: {} };
+const config: SocketIoConfig = { url: "http://192.168.4.1:5000", options: {} };
 
 @NgModule({
   imports: [
